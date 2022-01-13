a399a5bd56f7239a24385276d12715e8f29ea6b2
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-01-13 10:24:46 -0100

    created feign microservice and fraud client

---
+++ clients/src/main/java/com/ratz/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,13 @@
+package com.ratz.clients.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FraudCheckResponse {
+
+  private boolean isFraudulent;
+}
