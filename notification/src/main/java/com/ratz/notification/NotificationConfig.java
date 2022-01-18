1513be82363db84a135e240c7e6279db4878cfef
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-01-18 21:45:08 -0100

    added queue topic and binding

--- notification/src/main/java/com/ratz/notification/NotificationConfig.java
+++ notification/src/main/java/com/ratz/notification/NotificationConfig.java
@@ -1,6 +1,11 @@
 package com.ratz.notification;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,6 +19,28 @@ public class NotificationConfig {
 
   @Value("${rabbitmq.routing-keys}")
   private String internalNotificationRoutingKey;
+
+
+  @Bean
+  public TopicExchange internalTopicExchange() {
+
+    return new TopicExchange(this.internalExchange);
+  }
+
+  @Bean
+  public Queue notificationQueue() {
+
+    return new Queue(this.notificationQueue);
+  }
+
+  @Bean
+  public Binding internalToNotificationBinding(){
+
+    return BindingBuilder.bind(notificationQueue())
+        .to(internalTopicExchange())
+        .with(this.internalNotificationRoutingKey);
+  }
+
 
   public String getInternalExchange() {
     return internalExchange;
