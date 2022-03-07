a21ae455c38c0c92e5cf61e1b26c5a78bf8de733
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-03-07 13:50:23 -0100

    Tested rabbitMQ by sending messages in notification

--- notification/src/main/java/com/ratz/notification/NotificationApplication.java
+++ notification/src/main/java/com/ratz/notification/NotificationApplication.java
@@ -1,11 +1,24 @@
 package com.ratz.notification;
 
+import com.ratz.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ratz.notification", "com.ratz.amqp"})
 public class NotificationApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig){
+//        return args -> {
+//            producer.publish(new Person("Ratz", 33), notificationConfig.getInternalExchange(), notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 }
