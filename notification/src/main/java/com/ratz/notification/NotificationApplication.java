9a09c8e0d1800edffb7a51f47ad20c86d28c92ab
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-03-14 16:01:04 -0100

    created docker and kube profiles. removed eureka and api gateway

--- notification/src/main/java/com/ratz/notification/NotificationApplication.java
+++ notification/src/main/java/com/ratz/notification/NotificationApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(scanBasePackages = {"com.ratz.notification", "com.ratz.amqp"})
+@PropertySources({@PropertySource("classpath:clients-${spring.profiles.active}.properties")})
 public class NotificationApplication {
 
     public static void main(String[] args) {
